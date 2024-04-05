6bec4894a73ff2f3f97ed85f7470a4d7c07422f5
rasit60 <rasitanci@gmail.com>
2024-04-05 03:15:48 +0300

    .

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,13 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { CategoryPipe } from '../../pipes/category.pipe';
 import { CommonModule } from '@angular/common';
 import { ProductModel } from '../../models/product.model';
+import { ProductPipe } from "../../pipes/product.pipe";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule, CategoryPipe, CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  imports: [FormsModule, CategoryPipe, CommonModule, ProductPipe]
 })
 export class HomeComponent {
   categories: CategoryModel[] = [
@@ -31,24 +32,40 @@ export class HomeComponent {
   products: ProductModel[] = [
     {
       id: "1",
-      imageUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfFFXV4zCJybOFvocqAKKkko37SsPbl9F66Q&usqp=CAU',
-      name:'grunding pc',
-      description:"lorem ipsusu felna fırt",
-      price:12650,
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfFFXV4zCJybOFvocqAKKkko37SsPbl9F66Q&usqp=CA",
+      name: "Grundig Pc 2560 B1",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      price: 12650,
       discountedPrice: 11999,
-      stock:100,
-      kdvRate:20,
-      categoryId:"1",
-      category:{
-        id:"1",
-        name:"Elektronik"
+      stock: 100,
+      kdvRate: 20,
+      categoryId : "1",
+      category: {
+        id: "1",
+        name: "Elektronik"
       }
-
-}
+    },
+    {
+      id: "2",
+      imageUrl: "https://m.media-amazon.com/images/I/61ujQL9JflL._AC_SX679_.jpg",
+      name: "Columbia Delta Ridge™ Down Erkek Mont",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      price: 11550,
+      discountedPrice: 10999,
+      stock: 100,
+      kdvRate: 20,
+      categoryId : "3",
+      category: {
+        id: "3",
+        name: "Kıyafet"
+      }
+    }
+   
   ]
 
   categorySearch: string = ""
   selectedCategoryId: string = ""
+  productSearch: string = ""
 
 
   selectCategory(id: string) {
